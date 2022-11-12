90215ad62886019b86e1daeeeadd411240cd11ed
Jose Carrero <carrechez1@gmail.com>
2022-11-12 18:06:52 -0500

    se arreglo el bug de la paginacion

--- src/components/Pag.jsx
+++ src/components/Pag.jsx
@@ -1,40 +1,43 @@
 import React, { useState } from "react";
 
 const Pagination = ({ page, setPage, maximo, paginate, porPage }) => {
-  const [count, setCount] = useState(1);
   const nextPage = () => {
-    if (count < maximo) {
-      setCount(count + 1);
-      setPage(page + 1);
-    }
+    setPage(page + 1);
   };
   const previusPage = () => {
-    if (count > 1) {
-      setCount(count - 1);
-      setPage(page - 1);
-    }
+    setPage(page - 1);
   };
   const pageNumbers = [];
   for (let i = 1; i <= maximo; i++) {
     pageNumbers.push(i);
   }
+  const onSpecificPage = (n) => {
+    setPage(n);
+  };
   return (
     <div className="pag">
-      <button className="pag_b" onClick={previusPage}>
+      <button
+        className={`pag_b ${page === 1 ? "is-disabled" : ""}`}
+        onClick={previusPage}
+      >
         <i className="bx bxs-chevron-left-circle"></i>
       </button>
-      <nav className="pag_n">
-        <ul className="pag_ul">
-          {pageNumbers.map((number) => (
-            <li className="pag_li" key={number}>
-              <a className="pag_a" onClick={() => paginate(number)} href="!#">
-                {number}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <button className="pag_b" onClick={nextPage}>
+      <ul className="pag_ul">
+        {pageNumbers.map((number) => (
+          <li className="pag_li" key={number}>
+            <button
+              className={`pag_but ${number === page ? "is-current" : ""}`}
+              onClick={() => onSpecificPage(number, setPage(1))}
+            >
+              {number}
+            </button>
+          </li>
+        ))}
+      </ul>
+      <button
+        className={`pag_b ${page >= pageNumbers.length ? "is-disabled" : ""}`}
+        onClick={nextPage}
+      >
         <i className="bx bxs-chevron-right-circle"></i>
       </button>
     </div>
